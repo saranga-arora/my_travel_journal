e705b08d81a204be5f038bbb3925807ed2eff8d9
anonymous anonymous <saranga.arora@kellogg.northwestern.edu>
2022-03-09 18:35:11 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "journal_entries#index"
   resources :places
   resources :journal_entries
