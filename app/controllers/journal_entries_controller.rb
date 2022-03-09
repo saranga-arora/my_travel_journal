3ba28c282413b254402cd9b16125b4cd861fcafa
anonymous anonymous <saranga.arora@kellogg.northwestern.edu>
2022-03-09 18:36:33 +0000

    Remove controller comments

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -4,25 +4,20 @@ class JournalEntriesController < ApplicationController
 
   before_action :set_journal_entry, only: %i[show edit update destroy]
 
-  # GET /journal_entries
   def index
     @q = current_user.journal_entries.ransack(params[:q])
     @journal_entries = @q.result(distinct: true).includes(:user,
                                                           :place).page(params[:page]).per(10)
   end
 
-  # GET /journal_entries/1
   def show; end
 
-  # GET /journal_entries/new
   def new
     @journal_entry = JournalEntry.new
   end
 
-  # GET /journal_entries/1/edit
   def edit; end
 
-  # POST /journal_entries
   def create
     @journal_entry = JournalEntry.new(journal_entry_params)
 
@@ -38,7 +33,6 @@ class JournalEntriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /journal_entries/1
   def update
     if @journal_entry.update(journal_entry_params)
       redirect_to @journal_entry,
@@ -48,7 +42,6 @@ class JournalEntriesController < ApplicationController
     end
   end
 
-  # DELETE /journal_entries/1
   def destroy
     @journal_entry.destroy
     message = "JournalEntry was successfully deleted."
@@ -69,12 +62,10 @@ class JournalEntriesController < ApplicationController
     end
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_journal_entry
     @journal_entry = JournalEntry.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def journal_entry_params
     params.require(:journal_entry).permit(:user_id, :place_id, :title, :date,
                                           :description, :image)
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,6 @@
 class PlacesController < ApplicationController
   before_action :set_place, only: %i[show edit update destroy]
 
-  # GET /places
   def index
     @q = Place.ransack(params[:q])
     @places = @q.result(distinct: true).includes(:journal_entries,
@@ -9,24 +8,19 @@ class PlacesController < ApplicationController
     @location_hash = Gmaps4rails.build_markers(@places.where.not(location_latitude: nil)) do |place, marker|
       marker.lat place.location_latitude
       marker.lng place.location_longitude
-      marker.infowindow "<h5><a href='/places/#{place.id}'>#{place.name}</a></h5><small>#{place.location_formatted_address}</small>"
     end
   end
 
-  # GET /places/1
   def show
     @journal_entry = JournalEntry.new
   end
 
-  # GET /places/new
   def new
     @place = Place.new
   end
 
-  # GET /places/1/edit
   def edit; end
 
-  # POST /places
   def create
     @place = Place.new(place_params)
 
@@ -37,7 +31,6 @@ class PlacesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /places/1
   def update
     if @place.update(place_params)
       redirect_to @place, notice: "Place was successfully updated."
@@ -46,7 +39,6 @@ class PlacesController < ApplicationController
     end
   end
 
-  # DELETE /places/1
   def destroy
     @place.destroy
     redirect_to places_url, notice: "Place was successfully destroyed."
@@ -54,12 +46,10 @@ class PlacesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_place
     @place = Place.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def place_params
     params.require(:place).permit(:name, :location)
   end
