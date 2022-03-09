3e541502b4622e1c2e8475d0bd9d29e13112dd54
anonymous anonymous <saranga.arora@kellogg.northwestern.edu>
2022-03-09 18:35:15 +0000

    Whitelist columns in JournalEntry for ActiveAdmin

--- app/admin/journal_entries.rb
+++ app/admin/journal_entries.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register JournalEntry do
 
+ permit_params :user_id, :place_id, :title, :date, :description, :image
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
